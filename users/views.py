a44abe93d0dac81b57a95a2c95e9a7edacc9e730
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-08-22 19:12:23 +0100

    created forms.py to import to my views.py, started function to register users in views.py, will update again later. also created a register.html page to display registration form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from .forms import RegistrationForm
 
-# Create your views here.
+def register(request):
+
+    if request.method == 'Post':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = auth.authenticate(username=username, password=password)
+        return redirect('home')           
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html')
