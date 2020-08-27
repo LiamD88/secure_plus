56132402706b3a46e36c2c7ab6cea82f6e0bd7dc
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-08-27 23:54:55 +0100

    Added django widgets to classes for my forms to enable an easier layout for the login/register form  and attributes that makes fields required

--- users/forms.py
+++ users/forms.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-
 class RegistrationForm(UserCreationForm):
-   
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control' }))
+    username = forms.CharField(widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control' }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'required': 'True', 'class': 'form-control' }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control' }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control' }))
 
     class Meta:
         model = User
@@ -15,10 +19,8 @@ class RegistrationForm(UserCreationForm):
 
 class LoginForm(forms.Form):
     
-    username = forms.CharField(
-    required=True,)
+    username = forms.CharField(widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control' }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control' }))
 
-    password = forms.CharField(
-        required=True,
-    )
+
 
