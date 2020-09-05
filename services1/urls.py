a291ea33196b73c8fb3f5a78160e4506db05a943
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-09-05 17:29:54 +0100

    updated templates to include correctly spelling to render correclty, also added functions in my views.py to display new templates and created paths for theres templates in my urls.py file

--- services1/urls.py
+++ services1/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.services1, name='services1')
+    path('', views.services1, name='services1'),
+    path('oneoff', views.oneoff, name='oneoff'),
+    path('yearly', views.yearly, name='yearly'),
+    path('monthly', views.monthly, name='monthly')
 ]
--- services1/views.py
+++ services1/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def services1(request):
     return render(request, 'services1.html')
+
+def oneoff(request):
+    return render(request, 'oneoff.html')
+
+def yearly(request):
+    return render(request, 'yearly.html')
+
+def monthly(request):
+    return render(request, 'monthly.html')
