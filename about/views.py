4c4c72131c325c76ed3f2cbf3043fe420bd310ff
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-09-05 15:41:45 +0100

    I added a map to the bottom of the page to display the location of the business, running fine but replacing the api with one a variable from env.py causes the map to stop loading will need to update this

--- about/views.py
+++ about/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
+
 def about(request):
-    return render(request, 'about.html')
+
+    GOOGLE_MAPS_API_KEY = 'GOOGLE_MAPS_API_KEY'
+   
+    return render(request, 'about.html', {'GOOGLE_MAPS_API_KEY': GOOGLE_MAPS_API_KEY})
