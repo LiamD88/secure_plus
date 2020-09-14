25bcf4fe1c1d0b7a0f751c91c7982af4a6827a36
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-09-14 23:47:04 +0100

    amended path in url which was causing issue with travis build

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart, name='cart'),
+    path('', views.view_cart, name='view_cart'),
     path('add/<id>/', views.add_to_cart, name='add_to_cart'),
     path('amend/<id>/', views.amend_cart, name='amend_cart'),
 ]
