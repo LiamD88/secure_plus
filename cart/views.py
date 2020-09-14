2d01e75a70671ec481473dad982eaeacf5add1bd
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-09-14 23:34:07 +0100

    amended indentation

--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 def view_cart(request):
-    """ Render the main cart page"""
+    """ Render the main cart page """
     return render(request, 'cart.html')
 
 
@@ -29,15 +29,15 @@ def amend_cart(request, id):
 
     """ Ability to increase/decrease number of items in cart """
 
-        quantity = int(request.POST.get('quantity'))
-        cart = request.session.get('cart', {})
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
 
-        if quantity > 0:
-            cart[id] = quantity
-        else:
-            cart.pop(id)
+    if quantity > 0:
+    cart[id] = quantity
+    else:
+        cart.pop(id)
 
-        request.session['cart'] = cart
-        return redirect(reverse('cart'))
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
 
         
