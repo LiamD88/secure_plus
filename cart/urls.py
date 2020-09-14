9e78edba0a569fe37a526c1e89e552072eef4a02
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-09-14 23:07:55 +0100

    added path in urls.py for amend function

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add/<id>/', views.add_to_cart, name='add_to_cart'),
+    path('amend/<id>/', views.amend_cart, name='amend_cart'),
 ]
