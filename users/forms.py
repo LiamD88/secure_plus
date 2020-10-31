d1b85ebb6ea06eeb73ee445b675b05d03cdf111e
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-10-31 17:56:16 +0000

    added clean function to registration form to stop users registering with the same email or username as another user

--- users/forms.py
+++ users/forms.py
@@ -1,26 +1,42 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control' }))
-    username = forms.CharField(widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control' }))
-    email = forms.CharField(widget=forms.EmailInput(attrs={'required': 'True', 'class': 'form-control' }))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control' }))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control' }))
+    first_name = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Last Name'}))
+    username = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Username'}))
+    email = forms.CharField(label='', widget=forms.EmailInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Email'}))
+    password1 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Password'}))
+    password2 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Confirm Password'}))
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email is already in use.")
+        return email
+
+    
+    def clean_username(self):
+        username = self.cleaned_data('username')
+
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username is already in use.")
+        return username
+
 
 
 class LoginForm(forms.Form):
     
-    username = forms.CharField(widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control' }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control' }))
+    username = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Username' }))
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Password'}))
 
 
 
