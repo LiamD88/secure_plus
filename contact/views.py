be85c71c17f9e4a4ae3f05cfbe4d7a30c639ad20
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-11-07 20:03:03 +0000

    started backend code for contact form, not finished yet

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from .models import Contact
 
 def contact(request):
+
+    if request.method == 'POST':
+        name = request.POST('name')
+        message = request.POST('message')
+        email = request.POST('email')
+
+        
 
     return render(request, 'contact.html')
