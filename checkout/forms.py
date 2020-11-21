70c20890e68906a553b5b79fe77f10dfee380a1e
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-11-21 18:42:01 +0000

    added payment form

--- checkout/forms.py
+++ checkout/forms.py
@@ -16,3 +16,15 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'company_name', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'country', 'postcode')
+
+
+
+class PaymentForm(forms.Form):
+    credit_card_number = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Credit Card Number'}))
+    expiry_month = forms.ChoiceField(label='',choices=month_choice, widget=forms.Select(attrs={'required': 'True', 'class': 'form-control'}))
+    expiry_year = forms.ChoiceField(label='',choices=year_choice, widget=forms.Select(attrs={'required': 'True', 'class': 'form-control'}))
+    cvv= forms.NumberInput(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'CVV'}))
+
+    month_choice = [for i in range(1, 13)]
+    year_choice = [for i in range(2020, 2030)]
+    
