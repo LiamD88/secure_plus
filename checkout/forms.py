72e1d5b6042ad53848095304c0f728da7728ceeb
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-11-24 21:48:22 +0000

    added payment form to checkout app

--- checkout/forms.py
+++ checkout/forms.py
@@ -10,7 +10,7 @@ class OrderForm(forms.ModelForm):
     street_address2 = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Street Address 2'}))
     town_or_city = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Town/City'}))
     country = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Country'}))
-    postcode = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'False', 'class': 'form-control', 'placeholder': 'Country'}))
+    postcode = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'False', 'class': 'form-control', 'placeholder': 'Postcode'}))
 
 
     class Meta:
@@ -20,11 +20,14 @@ class OrderForm(forms.ModelForm):
 
 
 class PaymentForm(forms.Form):
+
+
+    month_choice = [(i, i) for i in range(1, 13)]
+    year_choice = [(i, i) for i in range(2020, 2030)]
+
     credit_card_number = forms.CharField(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'Credit Card Number'}))
     expiry_month = forms.ChoiceField(label='',choices=month_choice, widget=forms.Select(attrs={'required': 'True', 'class': 'form-control'}))
     expiry_year = forms.ChoiceField(label='',choices=year_choice, widget=forms.Select(attrs={'required': 'True', 'class': 'form-control'}))
-    cvv= forms.NumberInput(label='', widget=forms.TextInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'CVV'}))
+    cvv= forms.CharField(label='', widget=forms.NumberInput(attrs={'required': 'True', 'class': 'form-control', 'placeholder': 'CVV'}))
 
-    month_choice = [for i in range(1, 13)]
-    year_choice = [for i in range(2020, 2030)]
     
--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,58 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from .forms import OrderForm
+from .forms import OrderForm, PaymentForm
+from django. conf import settings
+from .models import OrderLineItem
+from services1.models import Package
+import stripe
+
+stripe.api_key = settings.STRIPE_PRIVATE
+stripe.api_key = settings.STRIPE_PUBLIC
+
+
 
 
 def checkout(request):
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('home'))
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+        payment_form = PaymentForm(request.POST)
 
-    order_form = OrderForm()
+
+
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save()
+            order.save()
+
+            cart = request.session.get('cart', {})
+            total = 0
+
+            for item_id, quantity in cart.items():
+                package = get_object_or_404(Package, pk=item_id)
+                total += quantity * package.price
+                order_line_item = OrderLineItem(order=order, package=package, quantity=quantity)
+                order_line_item.save()
+                
+        else:
+            messages.error(request, 'Payment not processed')
+
+    else:
+        order_form = OrderForm()
+        payment_form = PaymentForm()
+           
+
+
+   
     context = {
-        'order_form': order_form,
-    }
+            'order_form': order_form,
+            'payment_form': payment_form,
+            'public_key': settings.STRIPE_PUBLIC
+            }
     
 
 
     return render(request, 'checkout.html', context)
+
+
+
+
