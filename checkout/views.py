8595c0a991588d74bec65393e89b84212a6a8b1e
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-11-25 22:39:38 +0000

    i updated the checkout form to allow error and success messages

--- checkout/views.py
+++ checkout/views.py
@@ -21,7 +21,7 @@ def checkout(request):
 
 
         if order_form.is_valid() and payment_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
             order.save()
 
             cart = request.session.get('cart', {})
@@ -31,19 +31,25 @@ def checkout(request):
                 package = get_object_or_404(Package, pk=item_id)
                 total += quantity * package.price
                 order_line_item = OrderLineItem(order=order, package=package, quantity=quantity)
-                order_line_item.save()
-                
-            payment_stripe = stripe.PaymentIntent.create(
-                amount=int(total * 100),
-                currency=settings.STRIPE_CURRENCY
+                order_line_item.save()   
 
-            )
+                payment_stripe = stripe.PaymentIntent.create(
+                    amount=int(total * 100),
+                    currency=settings.STRIPE_CURRENCY)
 
 
-        else:
-            messages.error(request, 'Payment not processed')
+            messages.success(request, 'Your order has been placed')
+            return redirect(reverse('services1'))
+        
+        messages.error(request, 'This order has not been processed')
+        return redirect(reverse('services1'))
+
 
     else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "Theres nothing in your bag at the moment")
+            return redirect(reverse('services1'))
         
         order_form = OrderForm()
         payment_form = PaymentForm()
