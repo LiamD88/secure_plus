7c13857b43f6d085dee9a6f113115a2c8d0ce693
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-11-26 21:43:28 +0000

    updating to test google maps on heroku

--- about/views.py
+++ about/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
+
+GOOGLE_MAPS_API_KEY = settings.GOOGLE_MAPS_API_KEY
 
 
 def about(request):
@@ -6,7 +9,7 @@ def about(request):
     context = {
     
     'about': 'active',
-    'GOOGLE_MAPS_API_KEY' : 'GOOGLE_MAPS_API_KEY'
+    'GOOGLE_MAPS_API_KEY' : settings.GOOGLE_MAPS_API_KEY
     
     }
     
