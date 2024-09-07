e356891859783099e38ee95811adce270c906e05
LiamD88 <50365210+LiamD88@users.noreply.github.com>
2024-09-07 13:04:13 +0100

    updated allowed hosts

--- secure_plus/settings.py
+++ secure_plus/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['secure-plus.herokuapp.com',
+ALLOWED_HOSTS = ['secure-plus-6510e5d57aaa.herokuapp.com',
                  '127.0.0.1']
 
 
